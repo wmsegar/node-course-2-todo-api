55ea4895bf10f8b1739c8207408bb728d71201a5
wmsegar <wayne.segar@gmail.com>
2018-03-30 14:14:50 -0400

    Add POST /todos route and refactor

---
+++ server/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('./db/mongoose');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', function(req, res){
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then(function(doc){
+    res.send(doc);
+  }, function(e){
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, function(){
+  console.log('started on port 3000');
+});
