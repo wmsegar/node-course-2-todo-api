8153d475e9ac879efebd3533cb2dc9268c17320b
wmsegar <wayne.segar@gmail.com>
2018-03-30 15:07:00 -0400

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', function(req, res){
   });
 });
 
+app.get('/todos', function (req, res){
+  Todo.find().then(function(todos){
+    res.send({todos});
+  }, function(err){
+    res.status(400).send(e);
+  });
+});
+
 app.listen(3000, function(){
   console.log('started on port 3000');
 });
