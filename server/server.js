2415e07f6e8c93df97a403f3dd9accb295c488b1
wmsegar <wayne.segar@gmail.com>
2018-04-02 18:54:15 -0400

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var mongoose = require('./db/mongoose');
 var {User} = require('./models/user');
@@ -26,6 +27,22 @@ app.get('/todos', function (req, res){
     res.send({todos});
   }, function(err){
     res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', function(req, res){
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then(function(todo){
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(function(e){
+    res.status(400).send();
   });
 });
 
